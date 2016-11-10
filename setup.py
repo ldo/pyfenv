23fb1fe9545e16005675d8540e2fd4653e085bb2
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2016-11-10 04:41:10 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#+
+# Distutils script to install pyfenv. Invoke from the command line
+# in this directory as follows:
+#
+#     python3 setup.py build
+#     sudo python3 setup.py install
+#
+# Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+#-
+
+import distutils.core
+
+distutils.core.setup \
+  (
+    name = "pyfenv",
+    version = "0.5",
+    description = "access to additional functionality in C99 fenv.h, for Python 3.3 or later",
+    author = "Lawrence D'Oliveiro",
+    author_email = "ldo@geek-central.gen.nz",
+    url = "http://github.com/ldo/pyfenv",
+    py_modules = ["fenv"],
+  )
