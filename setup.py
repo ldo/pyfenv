3bdd6e585ee83da8920aac919e9b1fd843551c64
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2022-05-30 02:25:19 +0000

    Switch build script from soon-to-be-obsoleted distutils to recommended setuptools.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #+
-# Distutils script to install pyfenv. Invoke from the command line
-# in this directory as follows:
+# Setuptools script to install pyfenv. Make sure setuptools
+# <https://setuptools.pypa.io/en/latest/index.html> is installed.
+# Invoke from the command line in this directory as follows:
 #
 #     python3 setup.py build
 #     sudo python3 setup.py install
@@ -8,9 +9,9 @@
 # Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
 #-
 
-import distutils.core
+import setuptools
 
-distutils.core.setup \
+setuptools.setup \
   (
     name = "pyfenv",
     version = "0.5",
